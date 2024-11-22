1ea6d93963a6c7caac4f99a3603a6516cb4cf1a4
Martin Nammat <martinjens986@gmail.com>
2024-11-22 09:40:56 +0100

    add startup script

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from flask import send_file, Flask
 
 app = Flask(__name__)
 
+@app.route("/get/testing/startup")
+
+def return_files_tut_startup():
+	try:
+		return send_file("startup.sh",download_name="startup.sh",as_attachment=True)
+	except Exception as e:
+		return str(e)
 
 @app.route('/get/testing/receiver')
 def return_files_tut():
