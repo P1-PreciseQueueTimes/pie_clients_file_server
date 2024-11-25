f86f8f83a90f1c8ebe9cd0f7b8cc1f1e2437ccc8
Martin Nammat <martinjens986@gmail.com>
2024-11-25 08:57:06 +0100

    changed for automatic zip of pie clients. changed to be place to replace pie clients

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from flask import send_file, Flask
+from pathlib import Path
+import zipfile
+
+def zip_folder(folder_name,out_name):
+
+	fp_zip = Path(out_name)
+	path_to_archive = Path(folder_name)
+
+	with zipfile.ZipFile(fp_zip, "w", zipfile.ZIP_DEFLATED) as zipf:
+		for fp in path_to_archive.glob("**/*"):
+			zipf.write(fp, arcname=fp.relative_to(path_to_archive))
+
+zip_folder("test","test.zip")
 
 
 app = Flask(__name__)
@@ -14,6 +27,7 @@ def return_files_tut_startup():
 @app.route('/get/testing/receiver')
 def return_files_tut():
 	try:
+		zip_folder("pie_clients","pie_clients.zip")
 		return send_file("pie_clients.zip",download_name="pie_clients.zip",as_attachment=True)
 	except Exception as e:
 		return str(e)
--- test.py
+++
@@ -1,7 +0,0 @@
-
-import time
-import subprocess
-
-print(time.time()*1000)
-
-subprocess.run(["echo", "sui"])
