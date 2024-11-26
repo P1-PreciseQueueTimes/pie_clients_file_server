93d25cc2703cdb3837c1d011b7ef2efc3f435a3f
Martin Nammat <martinjens986@gmail.com>
2024-11-26 13:43:38 +0100

    updated sender websocket

--- pie_clients/sender.py
+++ pie_clients/sender.py
@@ -5,29 +5,42 @@ import socketio
 
 url_file = open("url.txt","r")
 
+def makeScan():
+        global request_number
+        subprocess.run(["wpa_cli" ,"-i" ,"wlan0", "scan"])
+        t = time.time_ns()
+
+        out_obj = {"request_number":request_number,"internal_time":t}
+
+        requests.post(post_url, json = out_obj)
+
+        request_number += 1
+
+
 base_url = url_file.read().strip() 
 
 post_url = base_url + "/post/testing/sender"
 
+automatic = False
+
 request_number = 0
 
-sio =socketio.SimpleClient()
+sio = socketio.Client()
+
+@sio.on("manual scan")
+def handle_manula_scan(data):
+	if not automatic:
+		makeScan()
+
+@sio.on("automatic scan")
+def handle_automatic_scan(data):
+	global automatic
+	automatic = not automatic
+	print("auto")
 
 sio.connect(base_url)
 
 while True:
-    subprocess.run(["wpa_cli" ,"-i" ,"wlan0", "scan"])
-    t = time.time_ns()
-    request_number += 1
-
-    out_obj = {"request_number":request_number,"internal_time":t}
-
-    requests.post(post_url, json = out_obj)
-
-    time.sleep(8)
-
-
-
-
-
-
+    if automatic == True:
+        makeScan()
+        time.sleep(8)
