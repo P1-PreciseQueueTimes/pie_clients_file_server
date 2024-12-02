3b00eeefd2f5d2fd02c6c1e11eb9c04eacbda6a0
Martin Nammat <martinjens986@gmail.com>
2024-12-02 13:05:05 +0100

    fixed everything

--- pie_clients/sender.py
+++ pie_clients/sender.py
@@ -1,72 +1,56 @@
-import pyshark
-import time
-import os
 import requests
+import time
+import subprocess
+import socketio
 import socket
 
 url_file = open("url.txt","r")
 
-base_url = url_file.read().strip() 
+def makeScan():
+        global request_number
+        subprocess.run(["wpa_cli","-i","wlan0","scan"])
+        t = time.time_ns()
 
-base_post_url = base_url + "/post/testing/receiver"
+        out_obj = {"request_number":request_number,"internal_time":t}
 
-base_post_start = base_url + "/post/testing/receiver/start"
+        requests.post(post_url, json = out_obj)
+
+        request_number += 1
 
 host_name = socket.gethostname()
 
-wifi_interface = "wlan1"
-sender_mac = "dc:a6:32:54:ac:ad"
-channel = 13
-user = ""
+base_url = url_file.read().strip() 
 
-set_time = False
+post_url = base_url + "/post/testing/sender"
 
-old_mac = ""
-old_time = 0
-
-capture = pyshark.LiveCapture(interface=wifi_interface)
+post_url_start = base_url + "/post/testing/sender/start"
 
 out_obj = {
     "host_name": host_name
 }
 
-requests.post(base_post_start, json=out_obj)
+requests.post(post_url_start, json=out_obj)
 
-def print_info(packet):
-    global old_mac, old_time,time_offset
-    current_time = time.time_ns() 
-    try:
-        if not packet["WLAN.MGT"] or not packet["WLAN"] or not packet["WLAN_RADIO"]:
-            return
-        if not packet["WLAN"].ta:
-            return
+automatic = False
 
-        if packet["WLAN"].fc_type_subtype == "0x0004":
-            if old_mac == packet["WLAN"].ta or (current_time - old_time) / 1000000.0 < 5000.0:
-                return
+request_number = 0
 
-            if packet["WLAN"].ta == sender_mac:
-                print("received signal")
-                signal_strength = packet["WLAN_RADIO"].signal_dbm
+sio = socketio.Client()
 
-                out_obj = {
-                    "host_name": host_name,
-                    "internal_time": current_time ,
-                    "signal_strength": signal_strength,
-                }
+@sio.on("manual scan")
+def handle_manula_scan(data):
+	if not automatic:
+		makeScan()
 
-                requests.post(base_post_url, json=out_obj)
+@sio.on("automatic scan")
+def handle_automatic_scan(data):
+	global automatic
+	automatic = not automatic
+	print("auto")
 
-                old_time = current_time
+sio.connect(base_url)
 
-            old_mac = packet["WLAN"].ta
-    except Exception:
-        pass
-
-
-out_str = f'airmon-ng start "{wifi_interface}" {channel} >/dev/null 2>&1'
-os.system("echo %s|sudo -S %s" % (user, out_str))
-
-for packet in capture.sniff_continuously():
-    print_info(packet)
-
+while True:
+    if automatic == True:
+        makeScan()
+        time.sleep(8)
