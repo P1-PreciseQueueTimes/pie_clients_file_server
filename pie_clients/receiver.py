b56d57b6e3aaba62fde7f705fd0176fac1731c8b
AFitBear <asbjornholmpedersen@gmail.com>
2024-12-10 13:00:18 +0100

    made backup to make receiver more powerfull. it can now scan all the mac-adresses.

--- pie_clients/receiver.py
+++ pie_clients/receiver.py
@@ -25,6 +25,8 @@ set_time = False
 old_mac = ""
 old_time = 0
 
+mac_table={}
+
 capture = pyshark.LiveCapture(interface=wifi_interface) #setup pyshark
 
 out_obj = {
@@ -37,9 +39,9 @@ def print_info(packet):
     global old_mac, old_time,time_offset
     current_time = time.time_ns() 
     try:
-        if not packet["WLAN.MGT"] or not packet["WLAN"] or not packet["WLAN_RADIO"]:
+        if not packet["WLAN.MGT"] or not packet["WLAN"] or not packet["WLAN_RADIO"]: #filters out wrong packages.
             return
-        if not packet["WLAN"].ta:
+        if not packet["WLAN"].ta: #checks if packet have a mac-adress.
             return
 
         if packet["WLAN"].fc_type_subtype == "0x0004":
@@ -48,21 +50,24 @@ def print_info(packet):
                 return
             """
 
-            if (current_time - old_time) / 1000000.0 < 5000.0:
+            if (packet["WLAN"].ta in mac_table):
+                if (current_time-mac_table[packet["WLAN"].ta])/ 1000000.0 < 5000.0:
+                    return
+            mac_table[packet["WLAN"].ta]=current_time
+
+            """if (current_time - old_time) / 1000000.0 < 5000.0:
                 return
-            if packet["WLAN"].ta == sender_mac:
-                print("received signal")
-                signal_strength = packet["WLAN_RADIO"].signal_dbm
+            if packet["WLAN"].ta == sender_mac: #checks mac-adress is the correct one."""
+            print("received signal")
+            signal_strength = packet["WLAN_RADIO"].signal_dbm
 
-                out_obj = {
-                    "host_name": host_name,
-                    "internal_time": current_time ,
-                    "signal_strength": signal_strength,
-                }
+            out_obj = {
+                "host_name": host_name,
+                "internal_time": current_time ,
+                "signal_strength": signal_strength,
+            }
 
-                requests.post(base_post_url, json=out_obj)
-
-                old_time = current_time
+            requests.post(base_post_url, json=out_obj) #sends data to server.
 
             old_mac = packet["WLAN"].ta
     except Exception:
@@ -72,6 +77,6 @@ def print_info(packet):
 out_str = f'airmon-ng start "{wifi_interface}" {channel} >/dev/null 2>&1' #linux command to setup antenna-sniffer
 os.system("echo %s|sudo -S %s" % (user, out_str)) #writes/executes command into terminal
 
-for packet in capture.sniff_continuously():
+for packet in capture.sniff_continuously(): #Everytime it catches a packet, it writes/procceses it. it doesen't stop.
     print_info(packet)
 
